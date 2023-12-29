750885afbadd968d702517a2478f415dd584736f
Gin_99 <3348483879@qq.com>
2023-12-29 17:59:55 +0800

    feat: 构建动态路由

--- src/store/modules/route.js
+++ src/store/modules/route.js
@@ -8,6 +8,23 @@ export default defineStore('routeStore', {
     }
   },
   actions: {
+    addRoutes(router) {
+      let modules = import.meta.glob("@/pages/**")
+      Object.keys(modules).forEach(path => {
+        const p = path.split('/').reverse()[1]
+        if (!skipRoute.includes(p)) {
+          this.routes.push({
+            path: '/' + p,
+            name: p,
+            component: modules[path],
+          })
+        }
+      })
+      console.log('this.routes', this.routes)
+      this.routes.forEach(m => router.addRoute(m))
+    }
   },
-  getters: {},
+  getters: {
+
+  },
 })
